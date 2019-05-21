17b3faaf30a39a00ded781c7fde512b72e121404
jcooperCUSD <jcooper@chicousd.org>
2019-05-21 08:41:27 -0700

    Initial Commit

---
+++ Clear-Groups.ps1
@@ -0,0 +1,57 @@
+<# 
+.SYNOPSIS
+ This function takes a group name, lists all current members, and removed the members from the supplied group name. . 
+.DESCRIPTION
+ A group or array of group names is operated on. Each group named will have all of its members removed from the group.
+.EXAMPLE
+ Clear-Groups -DomainController MyDC.mydomain -Groups Temp-Group1,Temp-Group2
+.INPUTS
+.OUTPUTS
+.NOTES
+#>
+function Clear-Groups {
+ [cmdletbinding()]
+ param ( 
+  [Parameter(Position=0,Mandatory=$True)]
+  [Alias('DC')]
+  [string]$DomainController,
+  [Parameter(Position=1,Mandatory=$True)]
+  [Alias('ADCred')]
+  [System.Management.Automation.PSCredential]$Credential,
+  [Parameter(Position=2,Mandatory=$True)]
+  [string[]]$Groups,
+  [Parameter(Position=3,Mandatory=$false)]
+  [SWITCH]$WhatIf
+ )
+ 
+ Begin{
+  . .\lib\Add-Log.ps1
+
+  if ( Test-Connection -ComputerName $DomainController) { 
+   $adCmdLets = 'Get-ADGroupMember','Remove-ADGroupMember'
+   $adSession = New-PSSession -ComputerName $DomainController -Credential $Credential
+   Import-PSSession -Session $adSession -Module ActiveDirectory -CommandName $adCmdLets -AllowClobber
+  } else {
+
+  }
+ }
+ Process{
+  foreach ($group in $Groups) { # BEGIN PROCESS GROUPS
+   Write-Verbose $group
+   $members = (Get-ADGroupMember -Identity $group).SamAccountName
+
+   if (!$members) { "No members in $group.";continue}
+
+   foreach ($sam in $members){
+    Add-Log remove "$group,$sam,member"
+   }
+   Remove-ADGroupMember -Identity $group -Members $members -Confirm:$false -WhatIf:$WhatIf
+  } # END PROCESS GROUPS
+ }
+
+ End{
+  'Tearing down sessions...'
+  Get-PSSession | Remove-PSSession -WhatIf:$false
+ }
+
+} # END
